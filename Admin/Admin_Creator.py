f65c494d58ee6548b8bc0ac5b7e27c571a594c5a
medve <medve@DESKTOP-IJSR4OU>
2025-12-11 10:08:21 -0800

    fix: fix double logic for admin and user

--- Admin/Admin_Creator.py
+++
@@ -1,8 +0,0 @@
-from DataBase.Base_User_Creator import BaseUserCreator
-
-class AdminCreator:
-    def __init__(self, dbname: str, user: str, password: str, host: str, port: str = "5432"):
-        self.base_creator = BaseUserCreator("admins", dbname, user, password, host, port)
-    
-    def create_admin(self, login: str, password: str, email: str, **extra_fields) -> bool:
-        return self.base_creator.create_account(login, password, email, **extra_fields)
